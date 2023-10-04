95c499d8d5d18677254485f3f38e94fede56472b
ChristinaManakkal <christinajosedaya@gmail.com>
2023-10-04 14:24:55 +0530

    Commiting

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,19 +1,60 @@
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
+// import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
+// import { Injectable } from "@angular/core";
+// import { JwtHelperService } from "@auth0/angular-jwt";
+
+
+// @Injectable({providedIn:  'root'})
+// export class AuthGuard {
+
+//     constructor(private router: Router) {
+//     }
+
+//     // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+//     //     if(state.url === '/welcome') {
+//     //         this.router.navigate(['login']);
+//     //         return false;
+//     //     }
+//     //     return true;
+//     // }
+
+//     canActivate(route : ActivatedRouteSnapshot, state : RouterStateSnapshot) {
+//         // return false;
+    
+//          if(!this.isAuthenticated() || this.isTokenExpired()){
+//             this.router.navigate(['login']);
+//             return false;
+//         }
+//         return true;
+//        }
+    
+//        isAuthenticated() : boolean {
+//         const token  = localStorage.getItem("token");
+//         return token != null || token != undefined;
+//        }
+    
+//        isTokenExpired() :boolean {
+//         const helper = new JwtHelperService();
+//         return helper.isTokenExpired(localStorage.getItem("token"));
+//        }
+
+// }
+
 import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, CanActivate } from "@angular/router";
+import { JwtService } from "./jwt.service";
 
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
 
-@Injectable({providedIn:  'root'})
-export class AuthGuard {
-
-    constructor(private router: Router) {
+    constructor(private router: Router, private jwtService: JwtService) {
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        if(state.url === '/welcome') {
+        const token = this.jwtService.getToken();
+        if (!token) {
             this.router.navigate(['login']);
             return false;
         }
         return true;
     }
-
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,37 +20,68 @@ export class LoginComponent {
     Password: new FormControl('', [Validators.required, Validators.minLength(3)])
   })
   
-  dummyToken: 'asw23sgd46hghgyfujgj';
+  //dummyToken: 'asw23sgd46hghgyfujgj';
 
   constructor(private router: Router, private employeeRestService: EmployeeRestService,  private loginRestService: LoginRestService) {
     this.IsAuthenticationFailed = false;
   }
 
-  login() {
+  // login() {
 
-    var logindto: any = {};
-    logindto.first_name = this.loginForm.controls.UserName.value;
-    logindto.dob = this.loginForm.controls.Password.value;
-    console.log(logindto)
-    this.loginRestService.signin(logindto).subscribe((response: any) =>   {
-        console.log('Logged In successfully!', response);
-        console.log(response.id);
-        if (response.id) {
-          this.IsAuthenticationFailed = false;      // Goes to the Html file to display the error if there is any
+  //   var logindto: any = {};
+  //   logindto.first_name = this.loginForm.controls.UserName.value;
+  //   logindto.dob = this.loginForm.controls.Password.value;
+  //   console.log(logindto)
+  //   this.loginRestService.signin(logindto).subscribe((response: any) =>   {
+  //       console.log('Logged In successfully!', response);
+  //       console.log(response.id);
+  //       if (response.id) {
+  //         this.IsAuthenticationFailed = false;      // Goes to the Html file to display the error if there is any
+  //         const id = response.id;
+  //         localStorage.setItem("token", JSON.stringify(this.dummyToken));
+  //         localStorage.setItem("id", JSON.stringify(id));
+  //         this.router.navigate(['welcome', id]);
+  //       } else {
+  //         console.log("Employee Not Found!");
+  //         this.IsAuthenticationFailed = true;      // Goes to the Html file to display the error if there is any
+  //         window.alert("Employee Not Found!");
+  //       }
+  //     },
+  //     (error) => {
+  //       this.IsAuthenticationFailed = true;
+  //       window.alert("Invalid User Credentials")
+  //       console.error('Error while Logged In!', error);
+  //     });
+  // }
+
+  login() {
+    const logindto: any = {
+      first_name: this.loginForm.controls.UserName.value,
+      dob: this.loginForm.controls.Password.value,
+    };
+  
+    this.loginRestService.signin(logindto).subscribe(
+      (response: any) => {
+        if (response.token) {
+          this.IsAuthenticationFailed = false;
           const id = response.id;
-          localStorage.setItem("token", JSON.stringify(this.dummyToken));
+          const token = response.token; // Use the actual field name "token" received from the server
+          localStorage.setItem("token", token); // Store the received token
           localStorage.setItem("id", JSON.stringify(id));
           this.router.navigate(['welcome', id]);
         } else {
+          this.IsAuthenticationFailed = true;
           console.log("Employee Not Found!");
-          this.IsAuthenticationFailed = true;      // Goes to the Html file to display the error if there is any
           window.alert("Employee Not Found!");
         }
       },
       (error) => {
         this.IsAuthenticationFailed = true;
-        window.alert("Invalid User Credentials")
+        window.alert("Invalid User Credentials");
         console.error('Error while Logged In!', error);
-      });
+      }
+    );
   }
+
+  
 }
